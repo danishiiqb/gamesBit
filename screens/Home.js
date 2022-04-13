7f0fd6f5df8ec1f434b0d2d60898c1ad1c564fb9
Danish Iqbal <danishiiqbal82@gmail.com>
2022-04-13 16:02:04 +0530

    added ScrollView and Home components and modfified onths functionality

--- screens/Home.js
+++ screens/Home.js
@@ -3,23 +3,43 @@ import React from 'react';
 import { SafeAreaView } from 'react-native';
 import { useEffect } from 'react';
 import Hero from '../components/Home.js/Hero';
+import List from '../components/Home.js/List';
+import { FlatList, ScrollView } from 'react-native';
+import ComingSoon from '../components/Home.js/ComingSoon/ComingSoon';
 
 const Home = () => {
-  useEffect(() => {
-    // async function getData() {
-    //   let gamesData = await (
-    //     await fetch(
-    //       `https://api.rawg.io/api/games?key=b4b44fe4937c40fe97bf7de6fb2d4c96`
-    //     )
-    //   ).json();
-    // }
-    // getData();
-  }, []);
+  function getModifiedMonth(val, opt = 1) {
+    const date = new Date();
+    const endingMonth = date.getMonth() + opt;
+    let startingMonth = endingMonth - val;
+    if (startingMonth < 0) {
+      startingMonth = 10 - startingMonth;
+    }
+    if (startingMonth === 0) {
+      startingMonth = 12;
+    }
+    return {
+      startingMonth: `${date.getFullYear()}-${`${0}${startingMonth}`.slice(
+        -2
+      )}-${date.getDate()}`,
+      endingMonth: `${date.getFullYear()}-${`${0}${endingMonth}`.slice(
+        -2
+      )}-${date.getDate()}`,
+    };
+  }
 
   return (
-    <View style={{ flex: 1, backgroundColor: '#1A002B' }}>
+    <ScrollView style={{ flex: 1, backgroundColor: '#1A002B' }}>
       <Hero></Hero>
-    </View>
+      <View style={{ flex: 1, marginTop: 15 }}>
+        <List
+          text={'Most Popular'}
+          months={{ startingMonth: '', endingMonth: '' }}
+        ></List>
+        <List text={'New Releases'} months={getModifiedMonth(1)}></List>
+        <ComingSoon months={getModifiedMonth(5, 9)}></ComingSoon>
+      </View>
+    </ScrollView>
   );
 };
 
