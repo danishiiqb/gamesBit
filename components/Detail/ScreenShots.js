fa23a6adfc3c4f2a14f9ff11c862a062a38ee881
Danish Iqbal <danishiiqbal82@gmail.com>
2022-04-14 16:48:16 +0530

    .

---
+++ components/Detail/ScreenShots.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const ScreenShots = () => {
+  return (
+    <View>
+      <Text>ScreenShots</Text>
+    </View>
+  );
+};
+
+export default ScreenShots;
