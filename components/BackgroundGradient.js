8c2b9ad860c94cdf9b17959b2e85bc1f7078be4d
Danish Iqbal <danishiiqbal82@gmail.com>
2022-04-15 23:30:22 +0530

    added coponents

---
+++ components/BackgroundGradient.js
@@ -0,0 +1,47 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+import { ImageBackground } from 'react-native';
+
+const BackgroundGradient = ({ image, height }) => {
+  return (
+    <View
+      style={{
+        height,
+        position: 'absolute',
+        width: '100%',
+        top: 0,
+        left: 0,
+      }}
+    >
+      <LinearGradient
+        start={[1, 0.6]}
+        end={[1, 1]}
+        style={{
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          zIndex: 56,
+        }}
+        colors={['transparent', 'rgba(26,0,43,1)']}
+      ></LinearGradient>
+      <ImageBackground
+        blurRadius={40}
+        style={{
+          top: 0,
+          left: 0,
+          width: '100%',
+          position: 'absolute',
+          height: '100%',
+          backgroundColor: '#2a0045',
+          resizeMode: 'cover',
+        }}
+        source={{
+          uri: image,
+        }}
+      ></ImageBackground>
+    </View>
+  );
+};
+
+export default BackgroundGradient;
---
+++ components/Detail/Description.js
@@ -0,0 +1,48 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { useState } from 'react';
+import GenericText from './GenericText';
+import { Pressable } from 'react-native';
+
+const Description = ({ desc }) => {
+  const [showMore, setShowMore] = useState(false);
+  return (
+    <View
+      style={{
+        marginTop: 35,
+      }}
+    >
+      <GenericText expand text={'Description'}></GenericText>
+      <Text
+        style={{
+          marginTop: 8,
+          color: 'white',
+          lineHeight: 20,
+          fontFamily: 'font-regular',
+        }}
+      >
+        {desc.slice(0, showMore ? desc.length : 300)}
+        {
+          <Pressable
+            onPress={() => {
+              setShowMore(!showMore);
+            }}
+          >
+            <Text
+              style={{
+                color: 'white',
+                marginTop: 24,
+                marginLeft: 12,
+                fontFamily: 'font-bold',
+              }}
+            >
+              {!showMore ? 'Show More...' : 'Show Less...'}
+            </Text>
+          </Pressable>
+        }
+      </Text>
+    </View>
+  );
+};
+
+export default Description;
