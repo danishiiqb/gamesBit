4acdc9e2e388abce44bd573a6c3d8a7fdd4a594f
Danish Iqbal <danishiiqbal82@gmail.com>
2022-04-16 23:22:48 +0530

    added cmponents

---
+++ screens/Collection.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Collection = () => {
+  return (
+    <View>
+      <Text>Collection</Text>
+    </View>
+  );
+};
+
+export default Collection;
---
+++ screens/Profile.js
@@ -0,0 +1,17 @@
+import { View, Text, SafeAreaView } from 'react-native';
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: '#1A002B',
+      }}
+    >
+      <Text>Profile</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Profile;
