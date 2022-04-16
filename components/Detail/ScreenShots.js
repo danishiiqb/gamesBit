205f7cad6e1dd5c4ef0e3dd86a9e4ddee4a18efc
Danish Iqbal <danishiiqbal82@gmail.com>
2022-04-16 23:24:20 +0530

    fixed styling

--- components/Detail/ScreenShots.js
+++ components/Detail/ScreenShots.js
@@ -1,10 +1,40 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import React from 'react';
+import ItemSoon from '../Home.js/ComingSoon/ItemSoon';
 
-const ScreenShots = () => {
+const ScreenShots = ({ images }) => {
   return (
-    <View>
-      <Text>ScreenShots</Text>
+    <View
+      style={{
+        marginTop: 11,
+        flexDirection: 'row',
+        height: '100%',
+      }}
+    >
+      <ItemSoon detail expanded item={images[1]}></ItemSoon>
+      <View
+        style={{
+          width: '50%',
+        }}
+      >
+        {images.slice(1, 4).map((img, idx) => {
+          if (idx === 0) {
+            return;
+          }
+          return (
+            <View
+              key={idx}
+              style={{
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+                height: '50%',
+              }}
+            >
+              <ItemSoon idx={idx} detail item={img}></ItemSoon>
+            </View>
+          );
+        })}
+      </View>
     </View>
   );
 };
