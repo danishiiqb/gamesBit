055cf0f696a7886f6da5ad31ec03d7386b290b3d
Danish Iqbal <danishiiqbal82@gmail.com>
2022-04-17 23:40:54 +0530

    added .

---
+++ components/BackBtn.js
@@ -0,0 +1,25 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/AntDesign';
+import { Pressable } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const BackBtn = () => {
+  const navigation = useNavigation();
+  return (
+    <Pressable
+      onPress={() => {
+        navigation.goBack();
+      }}
+      style={{
+        position: 'absolute',
+        top: 46,
+        left: 16,
+      }}
+    >
+      <Icon name='left' size={18} color='white'></Icon>
+    </Pressable>
+  );
+};
+
+export default BackBtn;
