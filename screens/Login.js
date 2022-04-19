9b19d88b336359f5f2c49e43885c5f7956a3a425
Danish Iqbal <danishiiqbal82@gmail.com>
2022-04-19 13:21:11 +0530

    fixed issues w/login.js connected to firebase

--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,16 @@
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView } from 'react-native';
 import React from 'react';
 import { TextInput } from 'react-native';
 import { useState } from 'react';
 import Button from '../components/Form/Button';
 import Seperator from '../components/Form/Seperator';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase';
+import BackBtn from '../components/BackBtn';
 
-const Login = () => {
+const Login = ({ navigation }) => {
+  const [load, setLoad] = useState(false);
+  const [err, setErr] = useState(null);
   const [formVal, setFormVal] = useState({
     email: '',
     password: '',
@@ -14,19 +19,47 @@ const Login = () => {
     setFormVal((prev) => {
       return { ...prev, [name]: e };
     });
+    setErr('');
   }
-
+  async function login() {
+    const { email, password } = formVal;
+    if (email && password) {
+      setLoad(true);
+      try {
+        let user = await signInWithEmailAndPassword(auth, email, password);
+        navigation.navigate('HomePage');
+      } catch (err) {
+        setErr(err.message);
+      }
+    } else {
+      setErr('Enter all fields');
+    }
+  }
+  function handleLogin() {
+    login().then((_) => {
+      setLoad(false);
+      setFormVal({
+        email: '',
+        password: '',
+      });
+    });
+  }
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
         backgroundColor: '#1A002B',
         justifyContent: 'center',
-        paddingLeft: 16,
-        paddingRight: 16,
+        position: 'relative',
       }}
     >
-      <View>
+      <BackBtn></BackBtn>
+      <View
+        style={{
+          paddingLeft: 16,
+          paddingRight: 16,
+        }}
+      >
         <View
           style={{
             marginBottom: 20,
@@ -41,6 +74,17 @@ const Login = () => {
           >
             Login
           </Text>
+          {err ? (
+            <Text
+              style={{
+                fontFamily: 'font-semibold',
+                color: 'red',
+                marginTop: 9,
+              }}
+            >
+              {err}
+            </Text>
+          ) : null}
         </View>
         <View>
           <TextInput
@@ -57,6 +101,7 @@ const Login = () => {
               color: 'white',
               borderRadius: 4,
             }}
+            value={formVal.email}
             textContentType='emailAddress'
             keyboardType='email-address'
             placeholderTextColor={'#bfbfbf'}
@@ -66,6 +111,7 @@ const Login = () => {
             onChangeText={(e) => {
               handleChange(e, 'password');
             }}
+            value={formVal.password}
             placeholderTextColor={'#bfbfbf'}
             secureTextEntry={true}
             style={{
@@ -83,22 +129,8 @@ const Login = () => {
             placeholder='Enter Password'
           ></TextInput>
           <Button
-            formSubmission={async function login() {
-              const { email, password } = formVal;
-              if (email && password) {
-                try {
-                  let user = await signInWithEmailAndPassword(
-                    auth,
-                    email,
-                    password
-                  );
-                  console.log(user);
-                } catch (err) {
-                  console.log(err.message);
-                }
-              }
-            }}
-            vals={formVal}
+            load={load}
+            formSubmission={handleLogin}
             text='Login'
             type='submit'
           ></Button>
@@ -106,7 +138,7 @@ const Login = () => {
           <Button route={'SignUp'} text='Sign Up' type='non-submit'></Button>
         </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
